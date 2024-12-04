0d2c4476248505148bc93b8b033db2010de81d1f
biancadizio <88462534+biancadizio@users.noreply.github.com>
2024-12-04 18:58:37 -0300

    Populando a tabela Contrato

--- src/dados.sql
+++ src/dados.sql
@@ -18,3 +18,6 @@ VALUES ('98.765.432/0001-12', 'Assessoria Jurídica');
 INSERT INTO Gestor_Esportivo (F_ID, Cidade, Endereco_Rua, Endereco_Numero, Endereco_Bairro, Endereco_CEP, RG, CPF, Nro_Carteira_Trabalho, Data_Contratacao, Telefone_1, Telefone_2)
 VALUES (1, '1234567', 'Rua A', 100, 'Centro', '01010-010', '12.345.678', '123.456.789-00', '12345', TO_DATE('01/01/2020', 'DD/MM/YYYY'), '11-98765-4321', '11-98888-4444');
 
+-- Populando a tabela Contrato
+INSERT INTO Contrato (ID_Contrato, Empresa, Gestor, Data, Orcamento, Periodo_Contrato)
+VALUES (1, '12.345.678/0001-90', 1, TO_DATE('01/03/2024', 'DD/MM/YYYY'), 50000, '12 meses');
