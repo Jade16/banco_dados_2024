bf587a9cdfc179f24d202228e3ffdc34d9ab634b
biancadizio <88462534+biancadizio@users.noreply.github.com>
2024-12-04 19:11:59 -0300

    Populando a tabela Espaco_Esportivo

--- src/dados.sql
+++ src/dados.sql
@@ -33,3 +33,8 @@ VALUES ('98.765.432/0001-01', 'Estádio Municipal', '1234567', 'Av. Principal', 
 INSERT INTO Funcionario_Instalacao (F_ID, Nome, CPF, RG, Nro_Carteira_Trabalho, Data_Contratacao, Instalacao_Esportiva, Telefone1, Telefone2)
 VALUES (1, 'Carlos Silva', '123.456.789-00', 'MG123456', '67890', TO_DATE('01/06/2020', 'DD/MM/YYYY'), '98.765.432/0001-01', '11-99999-8888', '11-98888-7777');
 
+-- Populando a tabela Espaco_Esportivo
+INSERT INTO Espaco_Esportivo (Instalacao, Nro_Espaco, Tipo)
+VALUES ('98.765.432/0001-01', 1, 'Campo de Futebol');
+INSERT INTO Espaco_Esportivo (Instalacao, Nro_Espaco, Tipo)
+VALUES ('98.765.432/0001-01', 2, 'Quadra de Vôlei');
