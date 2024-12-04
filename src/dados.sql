f58dc23bee6fa2e0d8e73fcd0d23e3c170a201b7
biancadizio <88462534+biancadizio@users.noreply.github.com>
2024-12-04 18:26:04 -0300

    Inserindo dados na tabela Cidade

--- src/dados.sql
+++ src/dados.sql
@@ -1,1 +1,6 @@
 -- script para inserir dados no banco já criado
+
+-- 1. Populando a tabela Cidade
+INSERT INTO Cidade (Codigo_Municipio, Nome, UF) VALUES ('1234567', 'São Paulo', 'SP');
+INSERT INTO Cidade (Codigo_Municipio, Nome, UF) VALUES ('7654321', 'Rio de Janeiro', 'RJ');
+INSERT INTO Cidade (Codigo_Municipio, Nome, UF) VALUES ('3456789', 'Belo Horizonte', 'MG');
