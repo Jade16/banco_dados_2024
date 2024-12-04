14a72f46fe5b0fcb0ee7be4f996915678b5c0431
biancadizio <88462534+biancadizio@users.noreply.github.com>
2024-12-04 18:56:27 -0300

    Populando a tabela Gestor_Esportivo

--- src/dados.sql
+++ src/dados.sql
@@ -14,4 +14,7 @@ VALUES ('98.765.432/0001-12', 'Treinamento de Equipe');
 INSERT INTO Servicos_Empresa (Empresa, Servico)
 VALUES ('98.765.432/0001-12', 'Assessoria Jurídica');
 
+-- Populando a tabela Gestor_Esportivo
+INSERT INTO Gestor_Esportivo (F_ID, Cidade, Endereco_Rua, Endereco_Numero, Endereco_Bairro, Endereco_CEP, RG, CPF, Nro_Carteira_Trabalho, Data_Contratacao, Telefone_1, Telefone_2)
+VALUES (1, '1234567', 'Rua A', 100, 'Centro', '01010-010', '12.345.678', '123.456.789-00', '12345', TO_DATE('01/01/2020', 'DD/MM/YYYY'), '11-98765-4321', '11-98888-4444');
 
