2e0231fda527783a7950b52857b73f2302b70336
Jade16 <jade_bortot_paiva@usp.br>
2024-12-05 20:34:09 -0300

    Atualização de quais consultas podemos fazer e marcação de qual será a primeira consulta

--- src/consultas.sql
+++ src/consultas.sql
@@ -1,8 +1,20 @@
 -- script com as consultas realizadas no banco de dados já criado
 
 -- ideias de consultas para se fazer:
-  -- as reservas aprovadas por um funcionário X ou qual funcionário aprovou uma reserva X
-  -- todas as reservas feitas por um usuario específico
-  -- quais contratos o gestor X realizou com uma empresa Y, ou com várias empresas
-  -- Empresas responsáveis pelas manutenções que ainda não foram realizadas
-  -- Exibir as cidades e os endereços das instalações esportivas que tem piscina
+  -- as reservas aprovadas por um funcionário X ou qual funcionário aprovou uma reserva X - join interno resolve
+    -- se quiser acrescentar mais algo, é possível ver quem é o usuário responsável pelo resultado anterior - acho que outro join interno possa resolver
+
+  -- todas as reservas feitas por um usuario específico, junto com as informações do espaço esportivo e instalação - acho que teria que ser uma consulta alinhada
+
+  -- quais as instalações esportivas e cidades de um usuário X já fez reservas - para isso acho que seria necessario um join externo 
+  
+  -- quais contratos o gestor X realizou com uma empresa Y, ou com várias empresas - join interno
+
+  -- Empresas responsáveis pelas manutenções que ainda não foram realizadas - consulta alinhada ?
+
+  -- Exibir as cidades e os endereços das instalações esportivas que tem piscina - 2 joins internos
+
+  -- buscar todas as reservas (esportivas e de manutenção) de um dia X - select 
+
+-- consulta 1: saber qual o funcionário que aprovou e o usuário responsável por todas as reservas esportivas que ainda não ocorreram
+
