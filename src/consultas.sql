df730d66d5c68f10ba2fdb30a3182c4f2286b015
Jade16 <jade_bortot_paiva@usp.br>
2024-12-05 19:53:20 -0300

    Documentação das possíveis consultas a se fazer

--- src/consultas.sql
+++ src/consultas.sql
@@ -1,1 +1,8 @@
 -- script com as consultas realizadas no banco de dados já criado
+
+-- ideias de consultas para se fazer:
+  -- as reservas aprovadas por um funcionário X ou qual funcionário aprovou uma reserva X
+  -- todas as reservas feitas por um usuario específico
+  -- quais contratos o gestor X realizou com uma empresa Y, ou com várias empresas
+  -- Empresas responsáveis pelas manutenções que ainda não foram realizadas
+  -- Exibir as cidades e os endereços das instalações esportivas que tem piscina
